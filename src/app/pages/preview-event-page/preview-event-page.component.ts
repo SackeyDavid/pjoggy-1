ca2b9fe4deaba64810de86933b2da69148d6d3a7
David <kofiahendev@gmail.com>
2021-08-13 06:03:17 +0000

    Speakers Section Added - section to display speakers imformation added

--- src/app/pages/preview-event-page/preview-event-page.component.ts
+++ src/app/pages/preview-event-page/preview-event-page.component.ts
@@ -112,19 +112,6 @@ export class PreviewEventPageComponent implements OnInit, AfterViewInit {
 
     
 
-    $(document).ready(function() {
-      
-      function stringToHslColor(str: string, s: string, l: string) {
-        var hash = 0;
-        for (var i = 0; i < str.length; i++) {
-          hash = str.charCodeAt(i) + ((hash << 5) - hash);
-        }
-        
-        var h = hash % 360;
-        return 'hsl('+h+', '+s+'%, '+l+'%)';
-      }
-      
-    });
 
 
 
